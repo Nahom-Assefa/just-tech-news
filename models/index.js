b3f1758e7be4b7aedad815b6eef7351bd18f6d25
Nahom Assefa <nzenebe@gmail.com>
2022-01-05 12:58:14 -0600

    12.3

--- models/index.js
+++ models/index.js
@@ -1,3 +1,30 @@
 const User = require('./User');
+const Post = require('./Post');
 
-module.exports = { User };
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'cascade',
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = { User, Post };
