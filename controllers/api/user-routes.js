fd535b95e8d446d405997714a454fdf42070ee2d
Nahom Assefa <nzenebe@gmail.com>
2022-01-13 12:22:27 -0600

    14.2

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -28,11 +28,11 @@ router.get("/:id", (req, res) => {
       },
       {
         model: Comment,
-        attributes: ['id', 'comment_text', 'created_at'],
+        attributes: ["id", "comment_text", "created_at"],
         include: {
           model: Post,
-          attributes: ['title']
-        }
+          attributes: ["title"],
+        },
       },
       {
         model: Post,
@@ -63,7 +63,15 @@ router.post("/", (req, res) => {
     email: req.body.email,
     password: req.body.password,
   })
-    .then((dbUserData) => res.json(dbUserData))
+    .then((dbUserData) => {
+      req.session.save(() => {
+        req.session.user_id = dbUserData.id;
+        req.session.username = dbUserData.username;
+        req.session.loggedIn = true;
+
+        res.json(dbUserData);
+      });
+    })
     .catch((err) => {
       console.log("post", err);
       res.status(500).json(err);
@@ -88,8 +96,27 @@ router.post("/login", (req, res) => {
       return;
     }
 
-    res.json({ user: dbUserData, message: "You are now logged in!" });
+    req.session.save(() => {
+      // declare session variables
+      req.session.user_id = dbUserData.id;
+      req.session.username = dbUserData.username;
+      req.session.loggedIn = true;
+
+      res.json({ user: dbUserData, message: "You are now logged in!" });
+    });
   });
+});
+
+router.post("/logout", (req, res) => {
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+    console.log("line 115, fucktology");
+  }
+  else {
+    res.status(404).end();
+  }
 });
 
 // PUT /api/users/1
--- server.js
+++ server.js
@@ -3,6 +3,20 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
+
+// This code sets up an Express.js session and connects the session to our Sequelize database.
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
 const app = express();
 const PORT = process.env.PORT || 3005;
@@ -17,6 +31,7 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
@@ -30,3 +45,11 @@ sequelize.sync({ force: false }).then(() => {
 
 
 
+// "dependencies": {
+//   "bcrypt": "^5.0.1",
+//   "dotenv": "^10.0.0",
+//   "express": "^4.17.2",
+//   "express-handlebars": "^6.0.2",
+//   "mysql2": "^2.3.3",
+//   "sequelize": "^7.0.0-alpha.4"
+// }
