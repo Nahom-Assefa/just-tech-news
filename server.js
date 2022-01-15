ea45f180a0944e9cd06ff8fac6eeeb132b616c9a
Nahom Assefa <nzenebe@gmail.com>
2022-01-14 22:28:24 -0600

    12.4

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
+const helpers = require('./utils/helpers');
 
 // This code sets up an Express.js session and connects the session to our Sequelize database.
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -22,7 +23,7 @@ const app = express();
 const PORT = process.env.PORT || 3005;
 
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
